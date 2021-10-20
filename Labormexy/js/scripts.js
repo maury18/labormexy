35f37c19fc152ac82bd039e6221ad98989c71410
maury18 <73630406+maury18@users.noreply.github.com>
2021-10-19 22:00:08 -0500

    Add files via upload

---
+++ Labormexy/js/scripts.js
@@ -0,0 +1,12 @@
+var check = document.querySelector(".check");
+check.addEventListener('click' , idioma);
+
+function idioma(){
+   let id=check.checked;
+   if (id==true){
+       location.href="../en/index.html";
+    } else {
+           location.href="../es/index.html";
+       }
+   
+}
